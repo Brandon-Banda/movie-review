3706541313d9516f5cad3a10311476acdd48168b
MC-874 <prime8486.mc@gmail.com>
2021-03-22 19:14:45 -0500

    Added my name for test.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
                 <h1> Init  🎉 </h1>
                 <p> Once again testing</p>
                 <p> I am once again making changes :) !</p>
+                <p> This is Max Clarke :)</p>
             </div>
         </div>
     );
