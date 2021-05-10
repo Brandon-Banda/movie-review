c5e48d88c7dc571c049e2f9d70b75ef82ce2aa7b
brandon-banda <brandonvzx@gmail.com>
2021-05-10 05:25:42 -0500

    testing stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Nav from "./Nav";
 import Activity from "./Activity";
 import * as htmlToImage from "html-to-image";
 import Navy from "./pages/Navy";
-import Images from "./images";
+import {useState} from 'react';
+
 function App() {
   var shareUrl = sessionStorage.getItem("export-url");
   return (
@@ -16,8 +17,8 @@ function App() {
           <div>
             <Nav />
             <Route path='/activity' component={Activity} />
-            <Route path='/' exact component={Home} />
-            <Route exact path='/test/:rateValue' component={Home} />
+            <Route path='/' exact component={() => <Movies />} />
+            <Route exact path='/:rateValue' component={() => <Movies />} />
             <Route exact path={`/share/${shareUrl}`} component={Export} />
           </div>
         </Switch>
@@ -25,8 +26,6 @@ function App() {
     </div>
   );
 }
-
-const Home = () => <Movies />;
 
 function Export() {
   var imgSrc = sessionStorage.getItem("export-rating");
@@ -40,6 +39,41 @@ function Export() {
 }
 
 const Movies = () => {
+
+  const [movies, setMovies] = useState([
+      {
+        idx: "1",
+        //title: "Inception",
+        /*img:
+          "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ1wNJi3WBo8wjZ-lxg4xPbg6-X7tQ1w6ZFI5L-RH1rUiOOGxLO",*/
+        lnk: "https://www.imdb.com/title/tt1375666/",
+      },
+      {
+        idx: "2",
+        title: "Interstellar",
+        img:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDg2ZptluiVtd9r5NaQndBw4Jb1QsVMSRPRXWGUXiO5pUKzRGC",
+      },
+      {
+        idx: "3",
+        title: "Star Wars Episode III: Revenge of the Sith",
+        img:
+          "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRjzq4ry-5lqWUO95DoVASJYiB5ZXGMOazC5qLJuEuZ3qxhQsJ5",
+      },
+      {
+        idx: "4",
+        title: "Lone Survivor",
+        img:
+          "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRPIHE-g2usJD26Hh5nJsdwN0aBiDQpumKkvGxGuZLrx17uBn2W",
+      },
+      {
+        idx: "5",
+        title: "Dexter",
+        img:
+          "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQKIxgh3Bxwrklou_UBFzMz-GCBZ5oL91TQMr2JHFCjWGbhsegj",
+      },
+  ]);
+
   function snap() {
     Share();
     // Screenshot
@@ -75,8 +109,8 @@ const Movies = () => {
 
   function removeMovies() {
     var removeIdx = 1;
-    console.log(Images[removeIdx].title);
-    Images[removeIdx].title.replace(Images[removeIdx].title, "");
+    console.log(movies[removeIdx].title);
+    movies[removeIdx].title.replace(movies[removeIdx].title, "");
     console.log(`Removed movie in row ${removeIdx}`);
     //functionality to remove the img, title, lnk.
   }
@@ -90,65 +124,67 @@ const Movies = () => {
 
   var tempPic = "https://cdn.onlinewebfonts.com/svg/img_51677.png";
 
+  const moviesList = movies.map(({ img, title, idx, lnk }) => (
+    <div className='container'>
+      <div className='imgContainer'>
+        <img
+          id={`moviePic${idx}`}
+          key={idx}
+          src={img ? img : tempPic}
+          alt='YE'
+        />
+      </div>
+      <h3>
+        {idx}.{" "}
+        <a href={lnk} target='_blank' rel='noreferrer'>
+          {title ? (
+            title
+          ) : (
+            <div className='menu'>
+              <button> Movie 1 </button>
+              <button> Movie 2 </button>
+              <button> Movie 3 </button>
+            </div>
+          )}
+        </a>
+      </h3>
+      <div className='change-btn'>
+        {title ? (
+          <div className='input-group-button'>
+            <button
+              type='button'
+              className='button hollow circle'
+              data-quantity='minus'
+              data-field='quantity'
+              onClick={removeMovies}
+            >
+              <i className='fa fa-minus' aria-hidden='true'></i>
+            </button>
+          </div>
+        ) : (
+          <div className='input-group-button'>
+            <button
+              type='button'
+              className='button hollow circle'
+              data-quantity='plus'
+              data-field='quantity'
+              onClick={addMovies}
+            >
+              <i className='fa fa-plus' aria-hidden='true'></i>
+            </button>
+          </div>
+        )}
+      </div>
+      <div className='rate-container'>
+        <Rate key={idx} count={movies.length + 1} />
+      </div>
+    </div>
+  ));
+
   return (
     <div>
       <div className='main' id='main'>
-        {Images.map(({ img, title, idx, lnk }) => (
-          <div className='container'>
-            <div className='imgContainer'>
-              <img
-                id={`moviePic${idx}`}
-                key={idx}
-                src={img ? img : tempPic}
-                alt='YE'
-              />
-            </div>
-            <h3>
-              {idx}.{" "}
-              <a href={lnk} target='_blank' rel='noreferrer'>
-                {title ? (
-                  title
-                ) : (
-                  <div className='menu'>
-                    <button> Movie 1 </button>
-                    <button> Movie 2 </button>
-                    <button> Movie 3 </button>
-                  </div>
-                )}
-              </a>
-            </h3>
-            <div className='change-btn'>
-              {title ? (
-                <div className='input-group-button'>
-                  <button
-                    type='button'
-                    className='button hollow circle'
-                    data-quantity='minus'
-                    data-field='quantity'
-                    onClick={removeMovies}
-                  >
-                    <i className='fa fa-minus' aria-hidden='true'></i>
-                  </button>
-                </div>
-              ) : (
-                <div className='input-group-button'>
-                  <button
-                    type='button'
-                    className='button hollow circle'
-                    data-quantity='plus'
-                    data-field='quantity'
-                    onClick={addMovies}
-                  >
-                    <i className='fa fa-plus' aria-hidden='true'></i>
-                  </button>
-                </div>
-              )}
-            </div>
-            <div className='rate-container'>
-              <Rate key={idx} />
-            </div>
-          </div>
-        ))}
+        {moviesList}
       </div>
       <button className='btn' onClick={snap}>
         Share
@@ -156,4 +192,5 @@ const Movies = () => {
     </div>
   );
 };
+
 export default App;
--- src/Rate.jsx
+++ src/Rate.jsx
@@ -3,24 +3,45 @@ import { useParams } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
 import "./styles.scss";
 
-const Rate = () => {
+const Rate = (props) => {
   const [rating, setRating] = useState(null);
+  const [counts, setCounts] = useState([
+    {index: 1, r: 3},
+    {index: 2, r: 1},
+    {index: 3, r: 5},
+    {index: 4, r: 2},
+    {index: 5, r: 4},
+  ]);
   let { rateValue } = useParams(); // should be 5 ratings for both, need to find a way to use the key of the rates
 
   useEffect(() => {
-    const data = localStorage.getItem("rating-state");
+    let data = [];
+    for(let i = 1; i < props.count; i++)
+    {
+      data+= localStorage.getItem(`rating-state${i}`);
+    }
+
     if (rateValue) {
       setRating(rateValue);
-      console.log(rateValue);
+      //console.log(rateValue);
     } else if (data && !rateValue) {
       // if there is rating data and no URL param
-      setRating(JSON.parse(data));
-      console.log("data  " + JSON.parse(data));
+      //setRating(JSON.parse(data)); this is wrong, we need to set each item in data to its respective localStorage section.
+      for(let i = 0; i < (props.count-1 ); i++)
+      {
+        setCounts({index: {i}, r: data});
+      }
+      console.log(counts);
+      //console.log("data  " + JSON.parse(data));
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("rating-state", JSON.stringify(rating));
+    for(let i = 0; i < props.count; i++)
+    {
+      localStorage.setItem(`rating-state${i}`, JSON.stringify(rating));
+    }
+    console.log('num of stars ' + (props.count -1))
   });
 
   return (
